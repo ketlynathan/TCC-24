6f0eae343d0d54bdb014d434f12660f6a1368b3b
Ketlyn Gonçalves <eukegoncalves@gmail.com>
2024-11-14 16:02:21 -0300

    Vesão 1

--- src/pages/2_Login.py
+++ src/pages/2_Login.py
@@ -6,6 +6,9 @@ import datetime
 from datetime import timedelta
 from rich import print as rprint
 from lib.http import create_authenticated_client
+import locale
+
+
 
 # Caminho da imagem da logo
 LOGO_PATH = "pages/img/Movimente-se.png"
@@ -33,10 +36,13 @@ def get_and_process_activities():
         # Create a DataFrame from the activities
         df = pd.DataFrame(activities)
 
+        # Configurar o idioma para português do Brasil
+        locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+
         # Process date and time columns
         df['start_date_local'] = pd.to_datetime(df['start_date_local'])
         df['date'] = df['start_date_local'].dt.date
-        df['month_str'] = df['start_date_local'].dt.strftime('%Y-%m')
+        df['month_str'] = df['start_date_local'].dt.strftime('%B %Y')  # Nome completo do mês em português
         df['time'] = pd.to_timedelta(df['moving_time'], unit='s')
 
         # Map activity types to Portuguese descriptions
@@ -188,6 +194,7 @@ def dashboard():
         st.sidebar.header("Filtros")
         selected_month = st.sidebar.selectbox("Selecione o Mês", df["month_str"].unique())
         selected_sport = st.sidebar.selectbox("Selecione o Tipo de Atividade", df["type_pt"].unique())
+        
 
         # Filter DataFrame based on selections
         filtered_df = df[(df["month_str"] == selected_month) & (df["type_pt"] == selected_sport)]
@@ -204,6 +211,7 @@ def dashboard():
             total_distance = filtered_df["distance"].sum() / 1000  # km
             total_time = filtered_df["time"].sum()
             total_elevation_gain = filtered_df["total_elevation_gain"].sum()
+            total_elevation_gain_str = f"{total_elevation_gain:.2f}"
             total_days = filtered_df["date"].nunique()
             days_in_month = filtered_df["start_date_local"].dt.days_in_month.iloc[0]
 
@@ -212,11 +220,11 @@ def dashboard():
             total_distance_str = f"{total_distance:.2f} km"
 
             # Display key metrics
-            col3, col4, col5, col6 = st.columns(4)
-            col3.metric("Distância Total", total_distance_str)
-            col4.metric("Tempo Total", total_time_str)
-            col5.metric("Ganho de Elevação Total", total_elevation_gain)
-            col6.metric("Dias de Atividade", total_days)
+            col1, col2, col3, col4 = st.columns(4)
+            col1.metric("Distância Total", total_distance_str)
+            col2.metric("Tempo Total", total_time_str)
+            col3.metric("Ganho de Elevação Total", total_elevation_gain_str)
+            col4.metric("Dias de Atividade", total_days)
 
             # Bar chart: number of days trained in the month
             days_not_trained = days_in_month - total_days
@@ -237,29 +245,123 @@ def dashboard():
                     autopct='%1.1f%%', startangle=90, colors=['#ff9999', '#66b3ff'])
             ax2.axis('equal')
 
+            st.markdown("---")
+
             # Displaying the charts side by side
-            col7, col8 = st.columns(2)
-            with col7:
+            col1, col2 = st.columns(2)
+            with col1:
                 st.pyplot(fig1)
-            with col8:
+            with col2:
                 st.pyplot(fig2)
+           
+        st.markdown("---")
+        #st.subheader("Velocidade Média por Tipo de Atividade")
+        fig2, ax = plt.subplots(figsize=(10, 6))
+        sns.boxplot(data=df, x="type_pt", y="average_speed", palette="viridis", ax=ax)
+        ax.set_xlabel("Tipo de Atividade")
+        ax.set_ylabel("Velocidade Média (m/s)")
+        ax.set_title("Distribuição de Velocidade Média por Tipo de Atividade")
+        #st.pyplot(fig2)
 
-        avg_speed_df = filtered_df.groupby("type_pt")["average_speed"].mean().reset_index()
+        col1, col2 = st.columns(2)
+        with col1:
+            #st.subheader("Progresso de Distância Comparado à Meta Mensal")
+            monthly_distance = df.groupby('month_str')['distance'].sum() / 1000  # Convertendo para km
+            meta_mensal = st.number_input("Defina sua meta mensal de distância (km)", min_value=0, max_value=500, value=100, step=10)
 
-            # Configuração da figura e do eixo
-        fig3, ax3 = plt.subplots(figsize=(12, 6))
-        sns.barplot(data=avg_speed_df, x="type_pt", y="average_speed", palette="viridis", ax=ax3)
+            st.write("Progresso Mensal com Meta de Distância")
+            fig1, ax = plt.subplots(figsize=(10, 6))
+            monthly_distance.plot(kind='line', marker='o', ax=ax, label="Distância Percorrida")
+            ax.axhline(meta_mensal, color='red', linestyle='--', label=f"Meta Mensal ({meta_mensal} km)")
+            ax.set_xlabel("Mês")
+            ax.set_ylabel("Distância (km)")
+            ax.legend()
+            st.pyplot(fig1)
+        with col2:
+            st.write("Distribuição de Velocidade Média por Tipo de Atividade")
+            st.pyplot(fig2)
 
-        # Customização dos rótulos e do título
-        ax3.set_ylabel('Velocidade Média (m/s)')
-        ax3.set_xlabel('Tipo de Atividade')
-        ax3.set_title('Velocidade Média por Tipo de Atividade')
+        # Renderizando o gráfico no Streamlit
+        
+        #st.subheader("Distribuição do Tipo de Atividade")
+        activity_counts = df['type_pt'].value_counts()
+        fig1, ax = plt.subplots()
+        ax.pie(activity_counts, labels=activity_counts.index, autopct='%1.1f%%', startangle=90, colors=sns.color_palette("viridis", len(activity_counts)))
+        ax.axis("equal")
+        #st.pyplot(fig1)
 
-            # Remoção das bordas do gráfico
-        sns.despine()
+        #st.subheader("Mapa de Calor de Dias Ativos")
+        df['day_of_week'] = df['start_date_local'].dt.day_name()
+        df['day'] = df['start_date_local'].dt.day
+        activity_counts = df.pivot_table(index='day_of_week', columns='day', values='id', aggfunc='count').fillna(0)
+        fig2, ax = plt.subplots(figsize=(10, 6))
+        sns.heatmap(activity_counts, cmap="YlGnBu", ax=ax, cbar_kws={'label': 'Atividades'})
+        #st.pyplot(fig2)
+        st.markdown("---")
 
-            # Exibição do gráfico no Streamlit
-        #st.pyplot(fig3)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Distribuição de Atividades por Tipo")
+            st.pyplot(fig1)
+        with col2:
+            st.write("Mapa de Calor de Dias Ativos")
+            st.pyplot(fig2)
+
+        #st.subheader("Atividades por Tipo e Mês")
+        monthly_activities = df.groupby(['month_str', 'type_pt']).size().unstack(fill_value=0)
+        fig1, ax = plt.subplots(figsize=(10, 6))
+        monthly_activities.plot(kind='bar', stacked=True, ax=ax, colormap="viridis")
+        ax.set_xlabel("Mês")
+        ax.set_ylabel("Número de Atividades")
+        #ax.set_title("Distribuição de Atividades por Tipo e Mês")
+        #st.pyplot(fig1)
+
+        #st.subheader("Distância Acumulada ao Longo do Tempo")
+        df['cumulative_distance'] = df['distance'].cumsum() / 1000  # Convertendo para km
+        fig2, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(df['start_date_local'], df['cumulative_distance'], color='blue', linewidth=2)
+        ax.set_xlabel("Data")
+        ax.set_ylabel("Distância Acumulada (km)")
+        #ax.set_title("Progresso de Distância ao Longo do Tempo")
+        #st.pyplot(fig2)
+
+        st.markdown("---")
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Distribuição de Atividades por Tipo e Mês")
+            st.pyplot(fig1)
+        with col2:
+            st.write("Progresso de Distância ao Longo do Tempo")
+            st.pyplot(fig2)
+
+
+        #st.subheader("Velocidade Média vs Distância")
+        fig1, ax = plt.subplots(figsize=(10, 6))
+        sns.scatterplot(data=df, x='distance', y='average_speed', hue='type_pt', palette="viridis", ax=ax)
+        ax.set_xlabel("Distância (m)")
+        ax.set_ylabel("Velocidade Média (m/s)")
+        #ax.set_title("Velocidade Média vs Distância por Tipo de Atividade")
+        #st.pyplot(fig1)
+
+        #st.subheader("Distribuição de Intensidade das Atividades")
+        fig2, ax = plt.subplots(figsize=(10, 6))
+        sns.histplot(df['distance'] / 1000, bins=20, kde=True, color="blue", ax=ax)  # Convertendo para km
+        ax.set_xlabel("Distância (km)")
+        #ax.set_title("Frequência de Atividades por Distância")
+        #st.pyplot(fig2)
+
+        st.markdown("---")
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Velocidade Média vs Distância por Tipo de Atividade")
+            st.pyplot(fig1)
+        with col2:
+            st.write("Frequência de Atividades por Distância")
+            st.pyplot(fig2)
+
+        
 
 
 if __name__ == "__main__":
--- src/pages/4_Planilha.py
+++ src/pages/4_Planilha.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 import datetime
+import numpy as np
 from io import BytesIO
+import plotly.express as px
 
 # Função para gerar o modelo de xlsx
 def gerar_modelo():
-    # Criar um DataFrame como exemplo de modelo
     dados = {
         "Tipo": ["Corrida"],
         "Data": ["03-11-2024"],
@@ -18,11 +19,10 @@ def gerar_modelo():
     }
     df_modelo = pd.DataFrame(dados)
     
-    # Salvar o DataFrame como xlsx em memória
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df_modelo.to_excel(writer, index=False, sheet_name='Modelo')
-    output.seek(0)  # Reinicia o ponteiro para o início do arquivo em memória
+    output.seek(0)
     
     return output
 
@@ -31,8 +31,6 @@ st.title("Controle de Treinos")
 
 # Opção para baixar o modelo de xlsx
 st.subheader("Baixar modelo")
-st.markdown("Clique no botão abaixo para baixar o modelo de arquivo em formato .xlsx")
-
 modelo = gerar_modelo()
 st.download_button(
     label="Baixar Modelo XLSX",
@@ -41,138 +39,119 @@ st.download_button(
     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 )
 
-# Opção para carregar o arquivo xlsx
-st.subheader("Carregar arquivo")
-st.markdown("Após preencher sua planilha conforme o modelo, carregue aqui o seu arquivo .xlsx")
-st.markdown("Carregue seu próprio arquivo .xlsx para visualização")
-
-# Carregar o arquivo
-uploaded_file = st.file_uploader("Escolha o arquivo", type=["xlsx"])
-
-# Exibir o conteúdo do arquivo carregado
-if uploaded_file is not None:
-    # Tentar importar o openpyxl e ler o arquivo
+# Função auxiliar para converter tempo em segundos
+def time_to_seconds(time_str):
     try:
-        df = pd.read_excel(uploaded_file, engine='openpyxl')
-        st.write("Conteúdo do arquivo carregado:")
-        #st.dataframe(df)
+        h, m, s = map(int, time_str.split(':'))
+        return h * 3600 + m * 60 + s
+    except (ValueError, AttributeError):
+        return 0
+
+# Carregar o arquivo de exemplo
+uploaded_file = st.file_uploader("Escolha o arquivo", type=["xlsx"])
+if uploaded_file:
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    
+    # Conversão e limpeza de dados
+    df['Data'] = pd.to_datetime(df['Data'], dayfirst=True, errors='coerce')
+    df['Ano'] = df['Data'].dt.year.astype('Int64')
+    df['Mes'] = df['Data'].dt.month_name(locale='pt_BR')  # Convertendo número do mês para o nome em português
+    df['Tempo_segundos'] = df['Tempo'].apply(time_to_seconds)
+    df['Distancia'] = df['Distancia'].astype(str).str.replace(',', '.', regex=False).astype(float)
+    df['Tempo_minutos'] = df['Tempo_segundos'] / 60  # Nova coluna em minutos
+
+    # Filtros de ano e mês
+    st.sidebar.header('Filtros')
+    ano_selecionado = st.sidebar.selectbox("Selecione o Ano", df["Ano"].dropna().unique())
+    mes_selecionado = st.sidebar.selectbox("Selecione o Mês", df["Mes"].dropna().unique())
+    df['Data'] = pd.to_datetime(df['Data'])
+
+    # Contar os dias de treino por mês
+    df['Mes'] = df['Data'].dt.to_period('M').astype(str)  # Convertemos Period para string
+    monthly_counts = df.groupby('Mes').size().reset_index(name='days_trained')
+
+    # Criar gráfico de barras
+    fig = px.bar(monthly_counts, x='Mes', y='days_trained', 
+                 title='Mês com mais dias treinados', 
+                 labels={'Mes': 'Mês', 'days_trained': 'Dias Treinados'},
+                 range_y=[0, 31])  # Definir o limite do eixo y de 0 a 31
+    # Exibir o gráfico no Streamlit
+    st.plotly_chart(fig)
+    
+    df['Mes_nome'] = df['Data'].dt.month_name(locale='pt_BR')  # Converte para o nome do mês para filtrar
+    df_filtrado = df[(df["Ano"] == ano_selecionado) & (df["Mes_nome"] == mes_selecionado)]
+    
+    # Métricas
+    if not df_filtrado.empty:
+        total_distancia = df_filtrado['Distancia'].sum()
+        media_tempo = str(datetime.timedelta(seconds=int(df_filtrado['Tempo_segundos'].mean())))
+        total_dias = df_filtrado['Data'].nunique()
+        maior_distancia = df_filtrado['Distancia'].max()
+        maior_distancia_data = df_filtrado.loc[df_filtrado['Distancia'].idxmax(), 'Data']
+        total_tempo = str(datetime.timedelta(seconds=int(df_filtrado['Tempo_segundos'].sum())))
+
+        # Cards de métricas principais
+        col1, col2, col3, col4 = st.columns(4)
+        col1.metric("Total de Distância", f"{total_distancia:.2f} KM")
+        col2.metric("Tempo Médio por Treino", media_tempo)
+        col3.metric("Dias Treinados", total_dias)
+        col4.metric("Maior Distância em um Dia", f"{maior_distancia:.2f} KM em {maior_distancia_data.strftime('%d-%m-%Y')}")
         
-        # Verificar se as colunas necessárias estão presentes no dataframe
-        if all(col in df.columns for col in ['Data', 'Tipo', 'Tempo', 'Titulo', 'Distancia']):
+        # Gráficos
+        # Gráfico de barras por tipo de atividade
+        tipo_distancia = df_filtrado.groupby('Tipo')['Distancia'].sum().reset_index()
+        tipo_tempo = df_filtrado.groupby('Tipo')['Tempo_segundos'].sum().reset_index()
+        tipo_tempo['Tempo_horas'] = tipo_tempo['Tempo_segundos'] / 3600
 
-            df['Data'] = pd.to_datetime(df['Data'], dayfirst=True, errors='coerce')
-            df['Ano'] = df['Data'].dt.year.astype('Int64')  # Converte o ano para int, lidando com NaNs
-            df['Mes'] = df['Data'].dt.month
-            df['Dia'] = df['Data'].dt.day
+        fig1, ax1 = plt.subplots(figsize=(10, 5))
+        sns.barplot(data=tipo_distancia, x='Tipo', y='Distancia', ax=ax1, color='skyblue')
+        ax1.set_ylabel("Distância Total (KM)")
+        ax2 = ax1.twinx()
+        sns.barplot(data=tipo_tempo, x='Tipo', y='Tempo_horas', ax=ax2, color='lightgreen')
+        ax2.set_ylabel("Tempo Total (Horas)")
+        
+        # Gráfico de linha para evolução de distância e tempo
+        df_filtrado = df_filtrado.sort_values('Data')
+        fig2, ax = plt.subplots(figsize=(10, 5))
+        sns.lineplot(data=df_filtrado, x='Data', y='Distancia', marker='o', label='Distância', ax=ax)
+        ax.set_ylabel("Distância (KM)")
+        ax2 = ax.twinx()
+        sns.lineplot(data=df_filtrado, x='Data', y='Tempo_segundos', marker='o', color='orange', label='Tempo', ax=ax2)
+        ax2.set_ylabel("Tempo (Segundos)")
+        
+        st.markdown("---")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Distância Total e Tempo Total por Tipo de Atividade")
+            st.pyplot(fig1)
+        with col2:
+            st.write("Evolução da Distância e Tempo por Data")
+            st.pyplot(fig2)
 
-            # Converter nomes dos meses para português
-            meses = {
-                1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril',
-                5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto',
-                9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro'
-            }
-            df['Mes_nome'] = df['Mes'].map(meses)
+        # Gráfico de calor por mês
+        df_calendario = df_filtrado.copy()
+        df_calendario['Mes'] = df_calendario['Data'].dt.month_name(locale='pt_BR')  # Usando o nome do mês em português
+        heatmap_data = df_calendario.pivot_table(index="Tipo", columns="Mes", values="Distancia", aggfunc="sum").fillna(0)
+        fig1, ax = plt.subplots(figsize=(10, 5))
+        sns.heatmap(heatmap_data, cmap="YlGnBu", annot=True, fmt=".2f", ax=ax)  # Adicionando duas casas decimais
+        ax.set_title("Frequência de Atividades por Mês")
+        
+        # Distribuição de tempos (em minutos) por mês
+        fig2, ax = plt.subplots(figsize=(10, 5))
+        sns.boxplot(data=df_filtrado, x="Mes", y="Tempo_minutos", ax=ax, palette="viridis")
+        ax.set_title("Distribuição de Tempos por Mês")
+        ax.set_ylabel("Tempo (em minutos)")
 
-            # Filtros de Ano e Mês
-            st.sidebar.header('Filtros')
-            ano_selecionado = st.sidebar.selectbox("Selecione o Ano", df["Ano"].dropna().unique())
-            mes_selecionado = st.sidebar.selectbox("Selecione o Mês", df["Mes_nome"].dropna().unique())
-            
-            # Filtrar dados por ano e mês selecionados
-            df_filtrado = df[(df["Ano"] == ano_selecionado) & (df["Mes_nome"] == mes_selecionado)]
-
-            if not df_filtrado.empty:
-                # Obter tipos de atividades
-                tipos_atividades = df_filtrado['Tipo'].unique()
-
-                # Contar dias treinados por tipo de atividade
-                dias_treinados = df_filtrado.groupby('Tipo')['Dia'].count().reset_index()
-                dias_treinados.columns = ['Tipo', 'Dias Treinados']
-                
-                # Dias treinados no total
-                total_dias_treinados = df_filtrado['Dia'].nunique()
-                total_provas = df_filtrado[df_filtrado['Titulo'] == 'Prova'].shape[0]
-
-                # Função para converter tempo em segundos
-                def time_to_seconds(time_str):
-                    try:
-                        h, m, s = map(int, time_str.split(':'))
-                        return h * 3600 + m * 60 + s
-                    except (ValueError, AttributeError):
-                        return 0  # Ignorar valores inválidos
-
-                # Calcular o total de tempo por atividade
-                total_tempo_por_tipo = {}
-                for tipo in tipos_atividades:
-                    tempos = df_filtrado[df_filtrado['Tipo'] == tipo]['Tempo'].tolist()
-                    total_segundos = sum(time_to_seconds(t) for t in tempos)
-                    total_tempo = str(datetime.timedelta(seconds=total_segundos))
-                    
-                    total_tempo_por_tipo[tipo] = total_tempo
-
-                # Gráfico de Pizza
-                fig_pizza, ax_pizza = plt.subplots()
-                ax_pizza.pie(dias_treinados['Dias Treinados'], labels=dias_treinados['Tipo'], autopct=lambda p: f'{int(p * sum(dias_treinados["Dias Treinados"]) / 100)}', startangle=90)
-                ax_pizza.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-                #3ax_pizza.set_title(f'Dias Treinados por Atividade em {meses[mes_selecionado]}/{ano_selecionado}')
-
-                # Convertendo e filtrando dados
-                df_filtrado.loc[:, 'Distancia'] = (
-                df_filtrado['Distancia']
-                .astype(str)
-                .str.replace(',', '.', regex=False)
-                .str.extract(r'(\d+\.\d+)')[0]
-                .astype(float)
-                )
-
-                # Filtra as distâncias maiores que 0 e calcula o total por tipo
-                df_kms = df_filtrado[df_filtrado['Distancia'] > 0]
-                total_kms_por_tipo = df_kms.groupby('Tipo')['Distancia'].sum().reset_index()
-
-                # Criando o gráfico de linhas
-                fig_line, ax_line = plt.subplots()
-                sns.lineplot(data=total_kms_por_tipo, x='Tipo', y='Distancia', marker='o', ax=ax_line)
-                ax_line.set_title('Total de KMs por Atividade')
-                ax_line.set_xlabel('Tipo de Atividade')
-                ax_line.set_ylabel('Total de KMs')
-
-                st.header('Resumo dos Treinamentos')
-                num_atividades = len(tipos_atividades)
-
-                if num_atividades <= 3:
-                    colunas = st.columns(num_atividades + 1)  # Coluna extra para "Dias Treinados" e "Provas"
-                    for i, tipo in enumerate(tipos_atividades):
-                        colunas[i].metric(tipo.capitalize(), total_tempo_por_tipo[tipo])
-                    colunas[-1].metric("Dias Treinados", total_dias_treinados)
-                    colunas[-1].metric("Provas", total_provas)
-                else:
-                    colunas1 = st.columns(4)  # Limite de 4 colunas na primeira linha
-                    colunas2 = st.columns(num_atividades - 4 + 1)  # Colunas restantes na segunda linha, mais uma para "Dias Treinados" e "Provas"
-    
-                    for i in range(4):
-                        colunas1[i].metric(tipos_atividades[i].capitalize(), total_tempo_por_tipo[tipos_atividades[i]])
-    
-                    for i in range(4, num_atividades):
-                        colunas2[i - 4].metric(tipos_atividades[i].capitalize(), total_tempo_por_tipo[tipos_atividades[i]])
-    
-                    colunas2[-1].metric("Dias Treinados", total_dias_treinados)
-                    colunas2[-1].metric("Provas", total_provas)
-
-                   
-                # Mostrar gráficos lado a lado
-                try:
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        st.pyplot(fig_pizza)
-                    with col2:
-                        st.pyplot(fig_line)
-                except Exception as e:
-                    st.error(f"Erro ao renderizar os gráficos: {e}")
-            else:
-                st.info("Não há dados disponíveis para o ano e mês selecionados.")
-        else:
-            st.error("O arquivo carregado não contém as colunas necessárias: 'Data', 'Tipo', 'Tempo', 'Titulo' e 'Distancia'.")
-    except ImportError:
-        st.error("O pacote 'openpyxl' não está instalado. Por favor, instale-o usando 'pip install openpyxl'.")
+        st.markdown("---")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Frequência de Atividades (Calendário)")
+            st.pyplot(fig1)
+        with col2:
+            st.write("Distribuição de Tempos por Mês")
+            st.pyplot(fig2)
+        
+    else:
+        st.info("Não há dados disponíveis para o ano e mês selecionados.")
 else:
     st.info("Nenhum arquivo carregado ainda.")
